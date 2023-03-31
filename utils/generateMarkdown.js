58ff470b569302bc46899d748a49925ffaf1f259
MegMathis <mcdonough.megan25@gmail.com>
2023-03-31 15:40:22 -0400

    generate_readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,7 +14,40 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  [![license](https://img.shields.io/badge/License-${data.license}-ff69b4)](https://shields.io)
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [License](#license)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## License
+  ${data.license}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  Any questions, please contact me at:
+  - GitHub:
+  [${data.github}](http://github.com/${data.github})
+  - Email:
+  [${data.email}](mailto:${data.email})`;
 }
 
 module.exports = generateMarkdown;
