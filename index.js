3e5532952262e4939e2283de39bd36b2827eca5f
MegMathis <mcdonough.megan25@gmail.com>
2023-03-31 15:38:00 -0400

    function_created_indexjs

--- index.js
+++ index.js
@@ -78,10 +78,22 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("README is generated");
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then(function (userInput) {
+    console.log(userInput);
+    writeToFile("README.md", generateMarkdown(userInput));
+  });
+}
 
 // Function call to initialize app
 init();
